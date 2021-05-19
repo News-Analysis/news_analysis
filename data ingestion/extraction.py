ee59e041034d5f8fd376e96e20d1d2e77e3d200e
kwjinwoo <jung003327@gmail.com>
2021-05-19 12:21:53 +0900

    modify

--- data ingestion/extraction.py
+++ data ingestion/extraction.py
@@ -6,25 +6,20 @@ path = glob.glob('./data/*')
 print(path)
 
 
-keyword = '홍준표'
+keyword = '재난지원금'
 ex = []
-
+press = []
 
 for p in path:
     df = pd.read_csv(p)
-    try:
-        content_mask = df['content'].str.contains(keyword)
-        content = df[content_mask]
-        title_mask = content['title'].str.contains(keyword)
-        title = content[title_mask]
-        ex.append(title)
-    except:
-        print('except')
-
-
-press = ['donganew.csv', 'hankyoreh.csv', 'joins.csv', 'josun.csv', 'mt_data.csv',
-         'pressian.csv', 'seoul_data.csv', 'trendnews.csv']
+    df.fillna('na', inplace=True)
+    press.append(df['press'][0])
+    content_mask = df['content'].str.contains(keyword)
+    content = df[content_mask]
+    title_mask = content['title'].str.contains(keyword)
+    title = content[title_mask]
+    ex.append(title)
 
 
 for t, p in zip(ex, press):
-    t.to_csv(keyword+'_'+p, encoding='utf-8-sig')
+    t.to_csv('./keyword/'+keyword+'/'+keyword+'_'+p+'.csv', encoding='utf-8-sig', index=False)
